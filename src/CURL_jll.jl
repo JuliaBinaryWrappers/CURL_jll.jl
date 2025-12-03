248ab19c78b4469a41316d1d7ed17d2c70ecb72e
jlbuild <juliabuildbot@gmail.com>
2025-12-03 16:16:17 +0000

    CURL_jll build 8.17.0+0

--- src/CURL_jll.jl
+++ src/CURL_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CURL")
-JLLWrappers.@generate_main_file("CURL", UUID("b21e61f3-bafc-59ac-ab14-4c5c62d6588d"))
+JLLWrappers.@generate_main_file("CURL", Base.UUID("b21e61f3-bafc-59ac-ab14-4c5c62d6588d"))
 end  # module CURL_jll
